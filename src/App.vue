4b545b1c261567c1f370323e25f7de3e928c5205
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-05-29 15:48:52 +0200

    add structure (header, main, footer)

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
+
 import { store } from "./store";
 
 export default {
@@ -7,10 +11,19 @@ export default {
       store,
     };
   },
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
 };
 </script>
 
-<template></template>
+<template>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+</template>
 
 <style lang="scss">
 @use "./assets/styles/general.scss" as *;
